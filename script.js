8cba0e4fe3c16cb9fa08f936b9ebf7536e8955aa
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-10 20:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let output = "";
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += "FizzBuzz\n";
+  } else if (i % 3 === 0) {
+    output += "Fizz\n";
+  } else if (i % 5 === 0) {
+    output += "Buzz\n";
+  } else {
+    output += i + "\n";
+  }
+}
+
+alert(output);
